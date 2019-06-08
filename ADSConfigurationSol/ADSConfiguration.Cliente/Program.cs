52eda1c54fa369c12cc25f639e9d2946567d237d
Rolando <angel.rolandop@gmail.com>
2019-06-08 18:59:49 -0500

    subir

---
+++ ADSConfigurationSol/ADSConfiguration.Cliente/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ADSConfiguration.Utilities;
+
+namespace ADSConfiguration.Cliente
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {         
+
+            var host = WebHost
+              .CreateDefaultBuilder(args)
+              .ConfigureAppConfiguration((builderContext, config) =>
+              {
+                  var env = builderContext.HostingEnvironment;
+                  config
+                       .SetBasePath(env.ContentRootPath)
+                       .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                       .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true); // optional extra provider
+                       
+
+                  config.AddADSConfiguration(env.EnvironmentName);
+                  config.AddEnvironmentVariables(); // overwrites previous values
+
+                  if (args != null)
+                  {
+                      config.AddCommandLine(args);
+                  }
+              })
+              .ConfigureLogging((hostingContext, logging) =>
+              {
+                  logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                  logging.AddConsole();
+                  logging.AddDebug();
+                  logging.AddEventSourceLogger();
+              })              
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
+        }
+
+     
+    }
+}
