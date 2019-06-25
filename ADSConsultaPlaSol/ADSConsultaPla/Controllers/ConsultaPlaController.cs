dbfe656d6b2d2574ca8af4ff9585e2d29e39c389
Cedeño Delgado Oscar Reimar <cedenoo@asegsur3.local>
2019-06-25 18:39:46 -0500

    Cambio en visualizacion de campos en json

--- ADSConsultaPlaSol/ADSConsultaPla/Controllers/ConsultaPlaController.cs
+++ ADSConsultaPlaSol/ADSConsultaPla/Controllers/ConsultaPlaController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using ADSConsultaPla.Models;
 using ADSConsultaPla.Services;
 using ADSUtilities;
+using ADSUtilities.Logger;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -80,35 +81,56 @@ namespace ADSConsultaPla.Controllers
         /// <param name="persona"></param>
         /// <returns></returns>
         [HttpPost("sisprev")]
-        public IActionResult GetConsultaSisprev(DatosClienteModel persona)
+        [ProducesResponseType(typeof(IActionResult), 200)]
+        [ProducesResponseType(typeof(IActionResult), 400)]
+        [ProducesResponseType(typeof(IActionResult), 404)]
+        [ProducesResponseType(typeof(IActionResult), 500)]
+        public IActionResult GetConsultaSisprev([FromBody] DatosClienteModel persona)
         {
             try
             {
-                _logger.LogInformation("Inicio Consulta Pla {@persona}", persona);
-                if (persona != null && string.IsNullOrEmpty(persona.Identification))
+                this.TraceId("GetConsultaSisprev_" + DateTime.Now.Ticks);
+                _logger.LogInformation("Inicio Consulta Sisprev {@persona}", persona);
+                if (ModelState.IsValid)
                 {
-                    _logger.LogError("Error al llamar GetConsultaSisprev: No tiene identificación");
-                    return this.ADSBadRequest();
-                }
-                else
-                {
-                    var result = _consultaPlaService.ConsultaSisprevServicePost(persona);
-                    if (result != null)
+                    if (persona != null && string.IsNullOrEmpty(persona.Identification))
                     {
-                        _logger.LogInformation("Respuesta Consulta Pla: {@persona} {@result}", persona, result);
-                        return this.ADSOk(result);
+                        _logger.LogError("Error al llamar GetConsultaSisprev: No tiene identificación");
+                        return this.ADSBadRequest();
                     }
                     else
                     {
-                        _logger.LogInformation("Respuesta consulta GetConsultaSisprev: {@persona} No encontrado", persona);
-                        return this.ADSNotFound();
+                        _logger.LogInformation("Inicio llamada servicio ConsultaSisprevServicePost: {@persona}", persona);
+                        var result = _consultaPlaService.ConsultaSisprevServicePost(persona);
+                        _logger.LogInformation("Fin llamada servicio ConsultaSisprevServicePost");
+                        if (result != null)
+                        {
+                            result.Observacion = persona.App;
+                            _logger.LogInformation("Respuesta Consulta Pla: {@persona} {@result}", persona, result);
+                            _logger.LogInformation("Fin Consulta Sisprev {@persona}", persona);
+                            return this.ADSOk(result);
+                        }
+                        else
+                        {
+                            _logger.LogInformation("Respuesta consulta GetConsultaSisprev: {@persona} No encontrado", persona);
+                            _logger.LogInformation("Fin Consulta Sisprev {@persona}", persona);
+                            return this.ADSNotFound();
+                        }
                     }
                 }
+                else
+                {
+                    _logger.LogError("Error en los datos enviados GetConsultaSisprev");
+                    _logger.LogInformation("Fin Consulta Sisprev {@persona}", persona);
+                    return this.ADSBadRequest();
+                }
+                
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error al llamar GetConsultaSisprev");
-                return this.ADSBadRequest();
+                _logger.LogError(ex, "Error Consulta Sisprev");
+                _logger.LogInformation("Fin Consulta Sisprev {@persona}", persona);
+                return this.ADSInternalError();
             }
         }
     }
